ec38f0d3aa2bf0a630fb0fa601c1a7754b1291b3
Devon Mcginnis <dmcginnis104@gmail.com>
2020-03-25 17:59:35 -0400

    Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        System.out.println("Enter a number! ");
+        Scanner input = new Scanner(System.in);
+
+        Integer usersNum = input.nextInt();
+
+        int sum = 0;
+
+        for(int i = 1; i <= usersNum; i++) {
+            int startSum = sum;
+            sum = sum + i;
+            System.out.println(startSum + " + " + i + " = " + sum);
+        }
+
+        System.out.println("The sum of your input is " + sum + ".");
 
     }
 }
